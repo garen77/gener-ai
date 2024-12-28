8fea98d724b458905dba8abc24850ca087caa7df
creo.cons <creo.cons@bene.it>
2024-12-28 10:29:36 +0100

    dfbfg

--- routes.py
+++ routes.py
@@ -1,6 +1,21 @@
-from flask import Blueprint
+from flask import (Blueprint, jsonify)
+from flask_cors import (CORS, cross_origin)
+from db import get_db
+from bson import json_util
 
 routeBp = Blueprint('routes',__name__)
+
+
+CORS(routeBp, resources={r"/*": {"origins": "*"}})
+
+@routeBp.route('/games-list', methods=['POST'])
+@cross_origin()
+def games_list():
+    db = get_db()
+    gamesCollection = db['game']
+    games = json_util.dumps(gamesCollection.find({}))
+    return games
+
 
 @routeBp.route('/')
 def home():
