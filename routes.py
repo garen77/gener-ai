29ef183f5e9a1dfc3e4fcdbeee697ee442cb6b45
creo.cons <creo.cons@bene.it>
2025-01-02 22:53:23 +0100

    sdfdfg

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import (Blueprint, jsonify)
+from flask import (Blueprint, request)
 from flask_cors import (CORS, cross_origin)
 from db import get_db
 from bson import json_util
@@ -16,7 +16,32 @@ def games_list():
     games = json_util.dumps(gamesCollection.find({}))
     return games
 
+@routeBp.route('/update-game-user', methods=['POST'])
+def updateGameUser():
+    db = get_db()
+    gamesCollection = db['game']
+    userCollection = db['user']
+    userName = request.form['userName']
+    gameName = request.form['gameName']
 
+    error = None
+    
+    if not userName:
+        error = 'User name is required.'
+    elif not gameName:
+        error = 'Game name is required.'
+    if error is None:
+        user = userCollection.find_one({"name": userName})
+        game = gamesCollection.find_one({"name": gameName})
+        if not user:
+            error = f"Utente con username {userName} inesistente"
+        elif not game:
+            error = f"Gioco {gameName} inesistente."
+        if not error:
+            gamesCollection.update_one({ 'name': gameName }, { '$push': {'users': user['_id']}})
+            return f"Utente {userName} censito con successo nel gioco {gameName} !!"
+        return error
+    
 @routeBp.route('/')
 def home():
     return 'Home'
